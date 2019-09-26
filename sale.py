047c11c6e5ef299b4717872897f5caac04b45ba9
Carlos G?lvez <carlos.galvez@nan-tic.com>
2019-09-26 15:00:33 +0200

    Fix incorrect call to rec_name

--- sale.py
+++ sale.py
@@ -155,7 +155,7 @@ class Sale(EdifactMixin):
             # if it isn't the true value because it will be calculated next
             # in the on_change_product and on_change_quantity calls.
             if not getattr(line, 'description'):
-                line.description = line.product.rec_name()
+                line.description = line.product.rec_name
             if not getattr(line, 'unit_price'):
                 line.unit_price = ZERO_
             else:
